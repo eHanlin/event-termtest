6afc44c25ff26f45c4db45c574946a4902d28638
Rodick.Huang <rodick@ehanlin.com.tw>
2018-03-01 18:53:00 +0800

    travis build test

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-if [[ ${TRAVIS_TAG} == *-SNAPSHOT ]]; then
+export TRAVIS_TAG="2.0.3-SNAPSHOT"
+if [[ "${TRAVIS_TAG}" == *-SNAPSHOT ]]; then
     API_HOST='test.ehanlin.com.tw'
 else
     API_HOST='www.ehanlin.com.tw'
 fi
 
+
+echo "TRAVIS_TAG => [$TRAVIS_TAG]"
+echo "API_HOST => [$API_HOST]"
 curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-termtest\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"termtest\"}" "https://${API_HOST}/event/api/Deploy"
