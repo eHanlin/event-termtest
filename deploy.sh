f3d3d33a84f2f36ae8fa95b48427938ee94f020b
Rodick.Huang <rodick@ehanlin.com.tw>
2018-03-05 10:44:33 +0800

    修改 travis shell 部署命令不支援 [[ 語法

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
-if [[ "${TRAVIS_TAG}" == *-SNAPSHOT ]]; then
-    API_HOST='test.ehanlin.com.tw'
-else
-    API_HOST='www.ehanlin.com.tw'
-fi
-
+#!/bin/sh
+case $TRAVIS_TAG in
+  *-SNAPSHOT ) API_HOST='test.ehanlin.com.tw';;
+  * ) API_HOST='www.ehanlin.com.tw';;
+esac
 
 echo "TRAVIS_TAG => [$TRAVIS_TAG]"
 echo "API_HOST => [$API_HOST]"
+echo $API_HOST
 curl -X POST -H 'Content-Type: application/json' -d "{\"Repository\":\"event-termtest\",\"Tag\":\"${TRAVIS_TAG}\",\"Owner\":\"eHanlin\",\"Password\":\"${EHANLIN_PW}\", \"Name\": \"termtest\"}" "https://${API_HOST}/event/api/Deploy"
