d5083d516fc9570096f65d11cc9af8990d5cb480
Rodick.Huang <rodick@ehanlin.com.tw>
2018-07-09 18:03:25 +0800

    修改 上傳 GCS 的 gulp 任務

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,27 @@ var path = require('path');
 var Q = require('q');
 var util = require('gulp-template-util');
 var less = require('less');
+var Storage = require('@google-cloud/storage');
+var gcs = new Storage({ projectId: "tutor-204108" })
+
+function uploadGCS() {
+    return es.map(function(file, cb) {
+        fs.stat(file.path, function(err, stats) {
+            if (stats.isFile()) {
+                gcs.bucket("tutor-events")
+                    .upload(file.path, {
+                        destination: `/event/termtest/${file.relative}`,
+                        public: true
+                    })
+                    .catch(err => {
+                        console.error('ERROR:', err);
+                    });
+            }
+
+            cb(null, file)
+        })
+    });
+}
 
 function buildStyle() {
     return es.map(function(file, cb) {
@@ -61,7 +82,6 @@ function copyStaticTask(dest) {
 
 function cleanTask() {
     return del([
-        'dist',
         'src/css'
     ]);
 }
@@ -87,5 +107,10 @@ gulp.task('package', function() {
     });
 
     return deferred.promise;
+});
+
+gulp.task("uploadGCS", function() {
+    return gulp.src(["dist/**/*"], {base: "dist"})
+            .pipe(uploadGCS());
 });
 
